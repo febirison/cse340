21d04257fd9e3d768fb186202e824d45b1eb136c
febirison <andersonokai@gmail.com>
2025-04-11 04:24:57 +0000

    Complete W05 Activity 2: JWT Authorization
    - Added checkLogin middleware to utilities/index.js.
    - Applied checkLogin to protect the account management route.
    - Tested with multiple account types (Client, Employee, Admin).

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -13,7 +13,7 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
 // Route to deliver the account management view
-router.get("/", utilities.handleErrors(accountController.buildManagement));
+router.get("/", utilities.checkLogin, utilities.handleErrors(accountController.buildManagement))
 
 // Process the registration data
 router.post(
