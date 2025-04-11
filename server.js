6be97eb334479ba23d834765476f295e94a6f4dc
febirison <andersonokai@gmail.com>
2025-04-11 01:43:01 +0000

    JSON Web Tokens and Cookies Introduced

--- server.js
+++ server.js
@@ -5,19 +5,18 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute.js")
-const session = require("express-session")
-const pool = require('./database/')
-const accountRoute = require("./routes/accountRoute")
-const bodyParser = require("body-parser")
-
-
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute.js");
+const session = require("express-session");
+const pool = require('./database/');
+const accountRoute = require("./routes/accountRoute");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser"); // Add cookie-parser
 
 /* ***********************
  * Middleware
@@ -31,53 +30,53 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   name: 'sessionId',
-}))
-
+}));
 
 // Express Messages Middleware
-app.use(require('connect-flash')())
+app.use(require('connect-flash')());
 app.use(function(req, res, next){
-  res.locals.messages = require('express-messages')(req, res)
-  next()
-})
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 // Body Parser Middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// Add cookie-parser middleware
+app.use(cookieParser());
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(static)
-app.use("/account", accountRoute)
-
+app.use(static);
+app.use("/account", accountRoute);
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
 
 /* ***********************
  * Index Route
  *************************/
-app.get("/", baseController.buildHome)
+app.get("/", baseController.buildHome);
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
@@ -95,7 +94,6 @@ app.use((req, res) => {
       message: "Sorry, we appear to have lost that page." 
   });
 });
-
 
 const intentionalErrorRoute = require("./routes/intentionalErrorRoute");
 app.use("/intentional-error", intentionalErrorRoute);
